0e5c3a8fa428df5d9ba84f69699c5f781e506e47
laxmigupta31111 <laxmi07114@gmail.com>
2025-04-11 00:02:58 +0530

    database and usermodel

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config(); // <-- Add this line before using process.env
+
+mongoose.connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
-app.listen(3000, ()=>{
-    console.log("server listing on port 3000...");
-
-})
+app.listen(3000, () => {
+  console.log("server listing on port 3000.....");
+});
