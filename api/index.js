f8fcd37582bffb3b1c8c5f3f953a009d52c74155
laxmigupta31111 <laxmi07114@gmail.com>
2025-04-11 12:01:27 +0530

    route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js'
 
 dotenv.config(); // <-- Add this line before using process.env
 
@@ -15,4 +16,9 @@ mongoose.connect(process.env.MONGO)
 const app = express();
 app.listen(3000, () => {
   console.log("server listing on port 3000.....");
+});
+
+app.use("/api/user",userRoutes);
+app.get('/', (req, res) => {
+  res.send('Welcome to the API root!');
 });
