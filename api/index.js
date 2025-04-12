b8cbe1a79a121a097d15e7ff22f92f52938ff8a4
laxmigupta31111 <laxmi07114@gmail.com>
2025-04-12 20:48:36 +0530

    add middleware

--- api/index.js
+++ api/index.js
@@ -25,3 +25,12 @@ app.get('/', (req, res) => {
   res.send('Welcome to the API root!');
 });
 app.use("/api/auth",authRoutes);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
