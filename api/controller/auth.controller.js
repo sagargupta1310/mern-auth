0860a65153ff55139dd2fed7e7522dbaa23abf10
laxmigupta31111 <laxmi07114@gmail.com>
2025-04-14 14:39:25 +0530

    create signin route api

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
-
+import { errorHandler } from '../utils/error.js'; // Update path if needed
+import jwt from 'jsonwebtoken';
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -11,5 +12,21 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     next(error);
     res.status(500).json(error.message);
+  }
+};
+
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, 'User not found'));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, 'wrong credentials'));
+    const {password: hashedPassword, ...rest}= validUser._doc;
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
+  } catch (error) {
+    next(error);
   }
 };
